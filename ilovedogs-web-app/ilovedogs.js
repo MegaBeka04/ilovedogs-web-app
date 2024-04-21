bb62aaa7458916ac97edc04f54cbc542cc4fba24
MegaBeka04 <163860995+MegaBeka04@users.noreply.github.com>
2024-04-21 16:45:21 +0500

    Add files via upload

---
+++ ilovedogs-web-app/ilovedogs.js
@@ -0,0 +1,53 @@
+function goToDogPage1() {
+    location.href = "affenpinscher.html";
+}
+
+function goToDogPage2() {
+    location.href = "afghan_hound.html";
+}
+
+function goToDogPage3() {
+    location.href = "airetale_terrier.html";
+}
+
+function goToDogPage4() {
+    location.href = "alaskan_malamute.html";
+}
+
+function goToDogPage5() {
+    location.href = "anatolian_shepherd.html";
+}
+
+function goToDogPage6() {
+    location.href = "australian_cattle.html";
+}
+
+function goToDogPage7() {
+    location.href = "australian_shepherd.html";
+}
+
+function goToDogPage8() {
+    location.href = "basset_fauve_de_bratagne.html";
+}
+
+function goToDogPage9() {
+    location.href = "bavarian_mountain_hound.html";
+}
+
+function goToDogPage10() {
+    location.href = "beagle.html";
+}
+
+function goToDogPage11() {
+    location.href = "belgian_shepherd_groenendael.html";
+}
+
+function goToDogPage12() {
+    location.href = "bichon_frise.html";
+}
+
+function search() {
+    var query = document.getElementById("searchInput").value;
+    console.log("Поиск: " + query);
+    return false;
+}
